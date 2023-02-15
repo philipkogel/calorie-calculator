30b0857dc9fd0f1c03e115b9f8873804f08b2271
philipkogel <philipkogel@yahoo.pl>
2023-02-15 18:06:38 +0100

    feat(CaloriesForm): boostrap from static files and input validation

--- app/models/CaloriesForm.py
+++ app/models/CaloriesForm.py
@@ -1,8 +1,18 @@
-from wtforms import StringField, FormField, IntegerField, FloatField, Form
-
+from wtforms import StringField, FormField, IntegerField, FloatField, Form, SubmitField
+from wtforms.validators import DataRequired
 class CaloriesForm(Form):
-    weight = FloatField(label='Weight')
-    height = FloatField(label='Height')
-    age = IntegerField(label='Age')
-    city = StringField(label='City')
-    country = StringField(label='Country')
+    height = FloatField(
+        label='Height',
+        validators=[DataRequired()],
+        description='Enter height in cm'
+    )
+    weight = FloatField(
+        label='Weight',
+        validators=[DataRequired()],
+        description='Enter weight in kg'
+    )
+    age = IntegerField(label='Age', validators=[DataRequired()])
+    city = StringField(label='City', validators=[DataRequired()])
+    country = StringField(label='Country', validators=[DataRequired()])
+
+    submit = SubmitField(label='Submit')
