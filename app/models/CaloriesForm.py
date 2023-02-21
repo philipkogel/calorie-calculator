8bd4cebc1f8757237117bd36caafde36e865f879
philipkogel <philipkogel@yahoo.pl>
2023-02-21 21:52:55 +0100

    feat(validators): numberranage validation added

--- app/models/CaloriesForm.py
+++ app/models/CaloriesForm.py
@@ -1,19 +1,25 @@
 from wtforms import StringField, FormField, IntegerField, FloatField, Form, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange, ValidationError
+
 
 class CaloriesForm(Form):
     height = FloatField(
         label='Height',
-        validators=[DataRequired()],
+        validators=[DataRequired(), NumberRange(min=1, max=250)],
         description='Enter height in cm'
     )
     weight = FloatField(
         label='Weight',
-        validators=[DataRequired()],
+        validators=[DataRequired(), NumberRange(min=1, max=600)],
         description='Enter weight in kg'
     )
-    age = IntegerField(label='Age', validators=[DataRequired()], description="20")
+    age = IntegerField(
+        label='Age',
+        validators=[DataRequired(), NumberRange(min=1, max=120)],
+        description="20"
+    )
     city = StringField(label='City', validators=[DataRequired()], description="Warsaw")
     country = StringField(label='Country', validators=[DataRequired()], description="Poland")
 
     submit = SubmitField(label='Submit')
+
